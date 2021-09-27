4df961b0808d82678e6f67003ce7c749cee8b709
Dmitry <tuma98@gmail.com>
2021-09-27 13:39:43 +0700

    Conditional website adding

--- ios/Classes/SwiftNativeContactPlugin.swift
+++ ios/Classes/SwiftNativeContactPlugin.swift
@@ -52,7 +52,10 @@ public class SwiftNativeContactPlugin: NSObject, FlutterPlugin, CNContactViewCon
         contact.givenName = dictionary["name"] as? String ?? ""
         contact.organizationName = dictionary["company"] as? String ?? ""
         contact.jobTitle = dictionary["jobTitle"] as? String ?? ""
-        contact.urlAddresses.append(CNLabeledValue(label:CNLabelURLAddressHomePage, value:NSString(string:(dictionary["website"] as? String ?? ""))))
+        if dictionary["website"] as? String {
+            contact.urlAddresses.append(CNLabeledValue(label:CNLabelURLAddressHomePage, value:NSString(string:(dictionary["website"] as String))))
+        }
+        
         if let avatarData = (dictionary["avatar"] as? FlutterStandardTypedData)?.data {
             contact.imageData = avatarData
         }
